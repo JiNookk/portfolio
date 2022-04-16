87e9289020f480e658c107431cc40b0f0b50cd25
wooooks <qnfdkf153@gmain.com>
2022-04-16 13:04:50 +0900

    JS final commit

--- main.js
+++ main.js
@@ -31,14 +31,13 @@ navbarMenu.addEventListener('click', (event) => {
   // return을 이용해 함수를 종료. else를 안써도 이런식으로 함수를 종료 가능
   if (link == null || undefined) {
     return;
-  } else {
-    console.log(link);
-    // const str = link.split('#');
-    // let element = document.getElementById(str[1]);
-    // querySelector로 어떠한 형태의 선택자도 받아올 수 있다!!!
-    scrollIntoView(link);
-    navbarMenu.classList.remove('clicked');
   }
+  // const str = link.split('#');
+  // let element = document.getElementById(str[1]);
+  // querySelector로 어떠한 형태의 선택자도 받아올 수 있다!!!
+  navbarMenu.classList.remove('clicked');
+  scrollIntoView(link);
+  selectNavItem(target);
 
   navbarMenuItem.forEach((navbarItem) => {
     if (link === navbarItem.dataset.link) {
@@ -47,6 +46,12 @@ navbarMenu.addEventListener('click', (event) => {
       navbarItem.classList.remove('active');
     }
   });
+
+  const options = {
+    root: null, //기본값으로 viewport(브라우저에 보여지는 윈도우 부분)
+    rootMargin: '0px', //viewport 바깥으로 감지영역을 넓힐때
+    threshold: 0.2, // 콜백함수를 언제 실행할지 정하는 기준. 0 > 요소 보이면 바로 실행, 1 > 요소 다 보이면 실행
+  };
 });
 
 // Navbar toggle button for small screen
@@ -146,4 +151,86 @@ workBtnContainer.addEventListener('click', (e) => {
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonial',
+  '#contact',
+];
+
+// map을 이용해서 섹션을 담고있는 새로운 배열을 반환받는다.
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+console.log(sections);
+console.log(navItems);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+  root: null,
+  rootMargin: '-20px',
+  threshold: 0.3,
+};
+
+// 옵저버 콜백 정의(우리가 실행시킬 내용) > entry에 section이 들어간다!
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      // 섹션의  인덱스를 가져오는 함수
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+
+      // 섹션의 인덱스를 반환 받은 후 방향을 결정
+      // (entry의 y좌표가 +인지 -인지 판단해서 방향결정)
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+// observer 변수에 옵저버 콜백과 옵션을 넣어서 옵저버 생성!
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+// wheel : 마우스로 직접 스크롤
+// scroll : 모든 스크롤을 포함.
+window.addEventListener('wheel', () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    Math.ceil(window.scrollY + window.innerHeight) ===
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+
+  selectNavItem(navItems[selectedNavIndex]);
+});
+
+// 배열화한 섹션관찰을 시작해줘!
+sections.forEach((section) => observer.observe(section));
+
+// finaltouch
+// 개발자 도구에 들어가서 ctrl shift c로 확인하면 어떤 클래스로 인해 프로퍼티가 적용되었는지 확인가능
+// 여기서 테스트를 해본 후 코드에서 변경하면 효율적
+// 개발자 도구에 스마트폰 모양을 누르면 스마트폰 화면크기로 볼 수 있다.
+// 개발자 도구를 참고하면 styles에 상위에 있을수록 우선순위임을 의미한다!
+// 가까운 프로퍼티가 먼 프로퍼티보다 우선순위 : cascading
+// html에서 배치를 바꾸지 않고도 flex 박스를 이용해서 위치를 변경할 수 있다.
+// window.addEventListener('scroll') ~ 을하면 굉장히 성능이 떨어지는 코드를 작동시킨다.
+// 콜백자체가 성능이 굉장히 떨어지기 때문이다.
